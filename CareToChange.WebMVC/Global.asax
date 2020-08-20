ddc596951197abfb587cd6fbd574814677f9752f
SperZ <ZachSperka@gmail.com>
2020-08-20 18:45:44 -0400

    Add project files.

---
+++ CareToChange.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CareToChange.WebMVC.MvcApplication" Language="C#" %>
---
+++ CareToChange.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CareToChange.WebMVC.Startup))]
+namespace CareToChange.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
